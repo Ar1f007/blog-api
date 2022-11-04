4b39417de2ed1e196a4ac8763008057ab9bcad85
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2022-11-05 02:14:14 +0600

    initial server setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Initial setup');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log('server is running'));
