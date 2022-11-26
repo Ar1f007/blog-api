63e55c1880d50ab333a139c4a92fc93402a6fe93
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2022-11-26 11:16:46 +0600

    create comment controller

--- src/controllers/comment/index.js
+++ src/controllers/comment/index.js
@@ -1,5 +1,25 @@
-const asyncWrapper = require('../../utils');
+const { asyncWrapper, AppError } = require('../../utils');
+const Comment = require('../../models/comment');
+const { StatusCodes } = require('http-status-codes');
 
-const createComment = asyncWrapper(async (req, res) => {});
+const createComment = asyncWrapper(async (req, res) => {
+  const { userId } = req.user;
+  const { postId, content } = req.body;
+
+  const comment = await Comment.create({
+    post: postId,
+    user: userId,
+    commentDesc: content,
+  });
+
+  if (!comment) {
+    throw new AppError('Could not create comment', StatusCodes.CONFLICT);
+  }
+
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    comment,
+  });
+});
 
 module.exports = { createComment };
--- src/routes/comment/index.js
+++ src/routes/comment/index.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 const middleware = require('../../middleware');
 const comment = require('../../controllers/comment');
 
-router.route('/').post(middleware.authenticateUser, comment.createComment);
+const { CreateCommentSchema } = require('../../validations/comment');
+
+router
+  .route('/')
+  .post(
+    middleware.authenticateUser,
+    middleware.validate(CreateCommentSchema, 'body'),
+    comment.createComment
+  );
+
+module.exports = router;
