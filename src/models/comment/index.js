24cb49e80b3170e276dced1c5097a1ffda7f7248
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2022-11-26 10:46:53 +0600

    comment model

---
+++ src/models/comment/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: [true, 'Post is required'],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Post is required'],
+    },
+    commentDesc: {
+      type: String,
+      required: [true, 'Comment description is something'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
