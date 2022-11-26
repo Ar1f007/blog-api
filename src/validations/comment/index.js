5173bc19d32b38eee11beb819aafd9022e59142c
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2022-11-26 12:18:01 +0600

    comment content schema

---
+++ src/validations/comment/index.js
@@ -0,0 +1,15 @@
+const { z } = require('zod');
+const { IdSchema } = require('../others');
+
+exports.CreateCommentSchema = z.object({
+  postId: IdSchema,
+  content: z.string().min(1),
+});
+
+exports.ParamIdSchema = z.object({
+  id: IdSchema,
+});
+
+exports.CommentContentSchema = z.object({
+  content: z.string(),
+});
