d8128375146834510654a4cc0fa0b0ae66224cd6
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-02-28 23:10:07 +0600

    category delete functionality

--- src/routes/category/index.js
+++ src/routes/category/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+const { ADMIN } = require('../../constants');
 const category = require('../../controllers/category');
+const middleware = require('../../middleware');
 
 router.route('/').get(category.getAllCategories);
+
+router
+  .route('/:id')
+  .delete(middleware.authorizePermission(ADMIN), category.deleteCategory);
 
 module.exports = router;
