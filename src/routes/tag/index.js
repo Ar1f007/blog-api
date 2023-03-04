9cc468181e55d401cb44ec458671c6db764ce386
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-03-05 00:13:03 +0600

    update tag linkup

--- src/routes/tag/index.js
+++ src/routes/tag/index.js
@@ -1,7 +1,18 @@
 const tag = require('../../controllers/tag');
-
+const { ADMIN } = require('../../constants');
+const middleware = require('../../middleware');
+const { NameSchema } = require('../../validations/others');
 const router = require('express').Router();
 
 router.route('/').get(tag.getAllTags);
+
+router
+  .route('/:id')
+  .put(
+    middleware.authenticateUser,
+    middleware.authorizePermission(ADMIN),
+    middleware.validate(NameSchema, 'body'),
+    tag.updateTag
+  );
 
 module.exports = router;
